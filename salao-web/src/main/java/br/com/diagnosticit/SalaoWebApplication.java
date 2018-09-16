cf2650165e72f13a515a2de9d77a8eb9f1f766a4
Cristiano <cacofut@hotmail.com>
2018-09-16 17:06:17 -0300

    Enviando a estrutura do projeto

---
+++ salao-web/src/main/java/br/com/diagnosticit/SalaoWebApplication.java
@@ -0,0 +1,12 @@
+package br.com.diagnosticit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaoWebApplication.class, args);
+	}
+}
