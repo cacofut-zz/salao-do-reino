1350bf5ef9c5f2c5002681415787ca1e6d488101
Cristiano <cacofut@hotmail.com>
2018-09-20 00:49:12 -0300

    astah

---
+++ salao-data/src/main/java/br/com/diagnosticit/model/Publisher.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.diagnosticit.model;
+
+/**
+ *
+ * @author cristiano
+ */
+public class Publisher {
+    
+    private Long id;
+    private String name;
+    private String lastName;
+
+    public Publisher() {
+        
+    }
+
+    public Publisher(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Publisher(Long id, String name, String lastName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+}
---
+++ salao-data/src/main/java/br/com/diagnosticit/service/CrudService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.diagnosticit.service;
+
+import java.util.Set;
+
+/**
+ *
+ * @author cristiano
+ */
+public interface CrudService <T, ID>{
+    
+    Set<T> findAll();
+    
+    T findById( ID id );
+    
+    T save( T object );
+    
+    void delete( T object );
+    
+    void deleteById( ID id );
+}
